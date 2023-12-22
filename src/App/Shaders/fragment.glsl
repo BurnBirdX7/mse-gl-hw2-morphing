a2f9d8834347ab27687b61ff24aac1aa382d9cb5
Artemiy Lazarev <artemiy.lazarevx7@gmail.com>
2023-12-23 00:05:16 +0300

    Added spot light

--- src/App/Shaders/fragment.glsl
+++ src/App/Shaders/fragment.glsl
@@ -4,17 +4,31 @@ uniform sampler2D tex_2d;
 
 in vec3 fragNormal;
 in vec2 fragTex;
-in vec3 fragPos;
-in vec3 fragDirectionalLightSource;
-in vec3 fragSpotLightDirection;
+in vec3 fragPos;                        // World space
+in vec3 fragDirectionalLightSource;     // View space
+in vec3 fragSpotLightDirection;         // View space
+in vec3 fragSpotLightRelativePosition;  // View space
 
 out vec4 outCol;
 
+vec4 get_spot() {
+    vec4 spotLightColor = vec4(0.6, 0.6, 0.9, 1);
+    float cos = dot(normalize(fragSpotLightRelativePosition), fragSpotLightDirection);
+    if (cos > 0 && acos(cos) < radians(20.0)) {
+        return spotLightColor;
+    }
+    return vec4(0);
+}
+
+
 void main() {
-    vec4 tex = texture(tex_2d, fragTex);
+    // Constants:
 
     float ambient = 0.2;
+    vec4 tex = texture(tex_2d, fragTex);
     float directional = max(dot(fragNormal, fragDirectionalLightSource), 0);
+    vec4 spot = get_spot();
 
-    outCol = (ambient + directional) * tex;
+
+    outCol = (ambient + directional + spot) * tex;
 }
--- src/App/Shaders/vertex.glsl
+++ src/App/Shaders/vertex.glsl
@@ -8,21 +8,41 @@ uniform mat4 mvp;
 uniform mat4 modelMat;   // Model to World transform
 uniform mat4 viewMat;    // World to View transform
 uniform mat4 normalMat;
+uniform float sphereBlend;
 
 out vec3 fragNormal;
 out vec2 fragTex;
-out vec3 fragPos;
-out vec3 fragDirectionalLightSource;
+out vec3 fragPos;					    // World space
+out vec3 fragDirectionalLightSource;    // View space
+out vec3 fragSpotLightDirection;	    // View space
+out vec3 fragSpotLightRelativePosition; // View space
+
+vec3 to_sphere(vec3 vertex) {
+	return vertex;  // TODO: Convert to sphere
+}
 
 void main() {
 	// Constants:
-	vec3 lightDir = vec3(1.0, 1.0, 1.0);
 	vec3 directionalLightSource = vec3(3.0, 5.0, 1.0);
+	vec3 spotLightSource = vec3(0.0, 5.0, 3.0);
+	vec3 spotLightDirection = vec3(0, 1, -2);
+
+	// Vertex:
+	vec4 vertex = vec4(mix(vertVertex, to_sphere(vertVertex), sphereBlend), 1.0);
+	// TODO: Morph normals
 
 	// Output:
-	fragNormal = normalize(mat3(normalMat) * vertNormal);
-	fragPos = vec3(modelMat * vec4(vertVertex, 1.0f));
+	fragPos = vec3(modelMat * vertex); 	  // World position
+	fragNormal = normalize(mat3(normalMat) * vertNormal); // World normals
+	fragTex = vertTex;									  // Texture coordinates
+
+	// Directional light:
 	fragDirectionalLightSource = normalize(mat3(viewMat) * directionalLightSource);
-	fragTex = vertTex;
-	gl_Position = mvp * vec4(vertVertex, 1.0);
+
+	// Spot light
+	fragSpotLightDirection = normalize(mat3(viewMat) * (spotLightDirection - spotLightSource));
+	fragSpotLightRelativePosition = mat3(viewMat) * vertex.xyz - mat3(viewMat) * spotLightSource;
+
+	// Postion output
+	gl_Position = mvp * vertex;
 }
