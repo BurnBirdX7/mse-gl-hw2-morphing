6b2d3976d88f45d9d70d52baf46c1a22ae793a3a
Artemiy Lazarev <artemiy.lazarevx7@gmail.com>
2023-12-23 03:47:08 +0300

    Morphing!

--- src/App/Shaders/vertex.glsl
+++ src/App/Shaders/vertex.glsl
@@ -16,8 +16,21 @@ out vec3 fragPos;					    // World space
 out vec3 fragSpotLightDirection;	    // View space
 out vec3 fragSpotLightRelativePosition; // View space
 
+float sphere_sqrt_component(vec2 sq) {
+	float core = 1 - sq.x / 2 - sq.y / 2 + sq.x * sq.y / 3;
+	return sqrt(core);
+}
+
 vec3 to_sphere(vec3 vertex) {
-	return vertex;  // TODO: Convert to sphere
+	vec3 squared = vertex * vertex;
+
+	vec3 sqrt = vec3(
+		sphere_sqrt_component(squared.yz),
+		sphere_sqrt_component(squared.xz),
+		sphere_sqrt_component(squared.xy)
+	);
+
+	return vertex * sqrt;
 }
 
 void main() {
@@ -26,12 +39,12 @@ void main() {
 	vec3 spotLightDirection = vec3(0, 1, -2);			// Direction
 
 	// Vertex:
-	vec4 vertex = vec4(mix(vertVertex, to_sphere(vertVertex), sphereBlend), 1.0);
-	// TODO: Morph normals
+	vec4 vertex = vec4(mix(vertVertex, to_sphere(vertVertex), 1.0), 1.0);
+	vec3 normal = mix(vertNormal, normalize(vertVertex), 1.0);
 
 	// Output:
 	fragPos = vec3(modelMat * vertex); 	 				  // World position
-	fragNormal = normalize(mat3(normalMat) * vertNormal); // World normals
+	fragNormal = normalize(mat3(normalMat) * normal); // World normals
 	fragTex = vertTex;									  // Texture coordinates
 
 	// Spot light
